98c0107526ae15823b769520ca456e21290267fa
lubaek <60066060+lubaek@users.noreply.github.com>
2022-04-11 16:34:51 +0600

    add good creation functionality

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from openpyxl import load_workbook
+
 
 app = Flask(__name__)
 
@@ -10,6 +11,20 @@ def homepage():
     sheet = excel['Sheet1']
     column = sheet['A']
     return render_template('index.html', goods=column)
+
+
+@app.route('/add/', methods=["POST"])
+def add():
+    good = request.form["good"]
+    excel = load_workbook('my_report.xlsx')
+    sheet = excel['Sheet1']
+    sheet.cell(row=sheet.max_row+1, column=1).value = good
+    print(sheet.max_row+1)
+    excel.save('my_report.xlsx')
+    return """
+        <h3>Инвертарь пополнен</h3>
+        <a href="/">Домой</a>
+    """
 
 
 if __name__ == "__main__":
